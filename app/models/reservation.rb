6baeeb0488a1f6418eadc86a5b9645a684819ec1
ju-to <088t3748@gmail.com>
2020-12-31 07:29:22 +0900

    rubyのバージョン変更及びBootstrapの導入#8

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,5 @@
 class Reservation < ApplicationRecord
   validates :user_name, :time, :menu_name, presence: true
-  validate :time,
+  # validate :time,
   validates :menu_name, length: { maximum: 100 }
 end
