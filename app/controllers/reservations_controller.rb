86b4787c064115304d085e3908157b3d78b4b800
ju-to <088t3748@gmail.com>
2021-01-04 06:38:17 +0900

    更新、削除機能を追加

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -12,13 +12,29 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    @reservation = Reservation.new(reservation_params)
+    reservation = Reservation.new(reservation_params)
     if @reservation.save
       redirect_to root_path, notice: "予約を登録しました"
     else
       flash.now[:alert] = "予約登録に失敗しました"
       render :new
     end
+  end
+
+  def update
+    reservation = Reservation.find(params[:id])
+    reservation.update(reservation_params)
+    redirect_to root_path, notice: "予約を更新しました"
+  end
+
+  def edit
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def destroy
+    reservation = Reservation.find(params[:id])
+    reservation.destroy
+    redirect_to root_path, notice: "予約を削除しました"
   end
 
   private
